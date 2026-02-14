2b44783dacb98970ffa54f423a7a856adae15e76
SrishanthreddyV <srishanthreddy1202@gmail.com>
2026-02-14 11:47:58 +0530

    netflix clone upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-<script>
   const languageToggle = document.getElementById('language-toggle');
   const languageLabel = document.querySelector('label[for="language-toggle"]');
   const languageListItems = document.querySelectorAll('.language-list li');
 
+  if (languageToggle && languageLabel && languageListItems.length > 0) {
   languageListItems.forEach(item => {
     item.addEventListener('click', () => {
-      languageLabel.textContent = item.textContent; // update label text
-      languageToggle.checked = false;               // close dropdown
-      // You can add code here to change site language as needed
+      languageLabel.textContent = item.textContent;
+      languageToggle.checked = false;
     });
   });
-</script>
+}
+
